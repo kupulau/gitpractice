364fc196481584a2556ab72c57a8adf74c90929a
kupulau <lavienrose0313@gmail.com>
2024-10-10 21:57:53 +0900

    refractor: Add error handle

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from typing import Union
 
-def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-    return x + y
+def add(x: Union[int, float], y: Union[int, float]) -> Optional[Union[int, float]]:
+    try:
+        result = x + y
+    except Exception as e:
+        print(e)
+        return None
+    return result
 
 if __name__ == "__main__":
     print(add(1, 2))
